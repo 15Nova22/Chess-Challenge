0f8f49983386c23c1dbca392915320d57941be11
15Nova22 <15-22novacom@web.de>
2023-07-26 16:14:21 +0200

    Schlagen für positiven Tausch

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -13,33 +13,39 @@ public class MyBot : IChessBot
     int lostPieces = 0;
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
+        Move[] allMoves = board.GetLegalMoves();
 
         //Array mit möglichen Zügen zum schlagen, wenn schlagen möglich ist, wird geschlagen
-        Move[] moves2 = board.GetLegalMoves(true);
-        foreach (Move schlagen in moves2)
-        {
-            if (!board.SquareIsAttackedByOpponent(schlagen.TargetSquare))
-            {
-                return schlagen;
-            }
-            else
-            {
-                int tempUnserValue = pieceValues[(int) schlagen.MovePieceType];
-                int tempSeinValue = pieceValues[(int) schlagen.CapturePieceType];
-                   
-                if(tempSeinValue > tempUnserValue)
-                {
-                    takenPieces += tempSeinValue;
-                    return schlagen;
-                }
-            }
-        }
+        Move[] captureMoves = board.GetLegalMoves(true);
 
+        //Alle Züge zum Schlagen Sortiert nach ihrem Wert Unterschied
+        Move bestCapture = captureMoves
+            //Nur positive Trades nehmen
+            .Where(move => TradeValue(board, move) > 0)
+            //Sortieren nach bestem Trade
+            .OrderBy(move => TradeValue(board, move)
+        //Zug mit bestem Wertverhältnis nehmen
+        ).FirstOrDefault();
+        //Valider Zug
+        if (!bestCapture.IsNull) return bestCapture;
         
+        //TODO Save attacked pieces
 
+        //Zufälliger Zug wenn sonst nichts machbar ist
         Random rng = new();
-        int temp = rng.Next(0, moves.Length);
-        return moves[temp];
+        return allMoves[rng.Next(allMoves.Length)];
+    }
+
+    /// <summary>
+    /// Calculates the Trade Value for a move
+    /// </summary>
+    /// <param name="board">The board</param>
+    /// <param name="move">The move to play</param>
+    /// <returns>
+    ///     The value difference between the capturing piece and the captured piece or just the value of the captured piece if no recapture is possible
+    /// </returns>
+    int TradeValue(Board board, Move move)
+    {
+        return pieceValues[(int)move.CapturePieceType] - (board.SquareIsAttackedByOpponent(move.TargetSquare) ? pieceValues[(int)move.MovePieceType] : 0);
     }
 }
