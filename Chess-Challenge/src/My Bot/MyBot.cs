d74b35fd5dca844d34a28a67613ee9a2d55104cc
tobiasfeldmann <132939959+tobiasfeldmann@users.noreply.github.com>
2023-07-27 16:40:44 +0200

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -11,9 +11,13 @@ public class MyBot : IChessBot
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
     int takenPieces = 0;
     int lostPieces = 0;
+    int counterRochade = 0;
+    String lastMove;
+    String[,] eroeffnung = new string[,] {  {"g1f3","b1c3","e2e4","d2d4","f1d3","c1e3","e1g1"} , {"g8f6", "b8c6","d7d5","e7e5","f8d6","c8e6","e8g8"}  };
     public Move Think(Board board, Timer timer)
     {
         Move[] allMoves = board.GetLegalMoves();
+        
 
         //Array mit möglichen Zügen zum schlagen, wenn schlagen möglich ist, wird geschlagen
         Move[] captureMoves = board.GetLegalMoves(true);
@@ -26,9 +30,29 @@ public class MyBot : IChessBot
             .OrderBy(move => TradeValue(board, move)
         //Zug mit bestem Wertverhältnis nehmen
         ).FirstOrDefault();
+
         //Valider Zug
-        if (!bestCapture.IsNull) return bestCapture;
-        
+        if (!bestCapture.IsNull) {
+            if (bestCapture.MovePieceType != PieceType.Pawn) {
+                lastMove = bestCapture.TargetSquare.Name + bestCapture.StartSquare.Name;
+            }
+            return bestCapture;
+        }
+
+        //Routine für Rochade, fester Ablauf, je nach Farbe
+        if (counterRochade < eroeffnung.Length) {
+            //Hat eine Figur während der Rochade geschlagen und wird vom Gegner bedroht, wird der Zug zurückgenommen
+            if (lastMove is not null && board.SquareIsAttackedByOpponent(new Square(lastMove.Substring(0,2)))) {
+                Move moveTemp = new Move(lastMove, board);
+                return moveTemp;
+            }
+            int farbe = board.IsWhiteToMove ? 0 : 1;
+            Move moveRochade = new Move(eroeffnung[farbe ,counterRochade], board);
+            counterRochade++;
+            return moveRochade;
+        }
+
+
         //TODO Save attacked pieces
 
         //Zufälliger Zug wenn sonst nichts machbar ist
